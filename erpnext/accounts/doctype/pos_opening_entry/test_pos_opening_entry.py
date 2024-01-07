48baaf69c9a2db920b93e59dcf2a009b4ecb0563
CodeAnt AI <chinmay@codeant.ai>
2024-01-07 17:27:10 +0000

    changed files

--- erpnext/accounts/doctype/pos_opening_entry/test_pos_opening_entry.py
+++ erpnext/accounts/doctype/pos_opening_entry/test_pos_opening_entry.py
@@ -7,21 +7,19 @@ import frappe
 
 
 class TestPOSOpeningEntry(unittest.TestCase):
-	pass
+    pass
 
 
 def create_opening_entry(pos_profile, user):
-	entry = frappe.new_doc("POS Opening Entry")
-	entry.pos_profile = pos_profile.name
-	entry.user = user
-	entry.company = pos_profile.company
-	entry.period_start_date = frappe.utils.get_datetime()
+    entry = frappe.new_doc("POS Opening Entry")
+    entry.pos_profile = pos_profile.name
+    entry.user = user
+    entry.company = pos_profile.company
+    entry.period_start_date = frappe.utils.get_datetime()
 
-	balance_details = []
-	for d in pos_profile.payments:
-		balance_details.append(frappe._dict({"mode_of_payment": d.mode_of_payment}))
+    balance_details = [frappe._dict({"mode_of_payment": d.mode_of_payment}) for d in pos_profile.payments]
 
-	entry.set("balance_details", balance_details)
-	entry.submit()
+    entry.set("balance_details", balance_details)
+    entry.submit()
 
-	return entry.as_dict()
+    return entry.as_dict()
